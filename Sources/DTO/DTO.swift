4386c744d2deec520326284651fab3e6d0701483
wsu212 <wsu212@gmail.com>
2024-07-04 09:39:28 -0400

    add login and register DTO

--- Sources/DTO/DTO.swift
+++ Sources/DTO/DTO.swift
@@ -1,2 +1,35 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
+
+/// Data Transfer Object for user registration.
+public struct RegisterResponseDTO: Codable {
+    public let error: Bool
+    public var reason: String? = nil
+    
+    public init(
+        error: Bool,
+        reason: String? = nil
+    ) {
+        self.error = error
+        self.reason = reason
+    }
+}
+
+/// Data Transfer Object for user login.
+public struct LoginResponseDTO: Codable {
+    public let error: Bool
+    public var reason: String? = nil
+    public var token: String? = nil
+    public var userID: UUID? = nil
+    
+    public init(
+        error: Bool,
+        reason: String? = nil,
+        token: String? = nil,
+        userID: UUID? = nil
+    ) {
+        self.error = error
+        self.reason = reason
+        self.token = token
+        self.userID = userID
+    }
+}
