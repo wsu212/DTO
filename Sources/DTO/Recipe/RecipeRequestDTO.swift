9e89cb9bcb74469a386eccef1aa58346200e2be2
Wei-Lun Su <48189264+wsu212@users.noreply.github.com>
2024-07-30 18:58:34 -0400

    create RecipeRequestDTO

---
+++ Sources/DTO/Recipe/RecipeRequestDTO.swift
@@ -0,0 +1,33 @@
+//
+//  RecipeRequestDTO.swift
+//
+//
+//  Created by Wei-lun Su on 7/30/24.
+//
+
+import Foundation
+
+public struct RecipeRequestDTO: Codable {
+    public let title: String
+    public let image: String
+    public let readyInMinutes: Int
+    public let servings: Int
+    public let sourceUrl: String
+    public let summary: String
+    
+    public init(
+        title: String,
+        image: String,
+        readyInMinutes: Int,
+        servings: Int,
+        sourceUrl: String,
+        summary: String
+    ) {
+        self.title = title
+        self.image = image
+        self.readyInMinutes = readyInMinutes
+        self.servings = servings
+        self.sourceUrl = sourceUrl
+        self.summary = summary
+    }
+}
