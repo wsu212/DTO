4ad3d1da0d1a995114ac105797f2c66698eddc2f
Wei-Lun Su <48189264+wsu212@users.noreply.github.com>
2024-07-31 10:47:52 -0400

    add RecipeResponseDTO

---
+++ Sources/DTO/Recipe/RecipeResponseDTO.swift
@@ -0,0 +1,42 @@
+//
+//  RecipeResponseDTO.swift
+//  
+//
+//  Created by Wei-lun Su on 7/31/24.
+//
+
+import Foundation
+
+public struct RecipeResponseDTO: Codable, Sendable {
+    public let id: UUID
+    public let title: String
+    public let image: String
+    public let readyInMinutes: Int
+    public let servings: Int
+    public let sourceUrl: String
+    public let summary: String
+    public let extendedIngredients: [IngredientDTO]
+    public let analyzedInstructions: [InstructionDTO]
+    
+    public init(
+        id: UUID,
+        title: String,
+        image: String,
+        readyInMinutes: Int,
+        servings: Int,
+        sourceUrl: String,
+        summary: String,
+        extendedIngredients: [IngredientDTO],
+        analyzedInstructions: [InstructionDTO]
+    ) {
+        self.id = id
+        self.title = title
+        self.image = image
+        self.readyInMinutes = readyInMinutes
+        self.servings = servings
+        self.sourceUrl = sourceUrl
+        self.summary = summary
+        self.extendedIngredients = extendedIngredients
+        self.analyzedInstructions = analyzedInstructions
+    }
+}
